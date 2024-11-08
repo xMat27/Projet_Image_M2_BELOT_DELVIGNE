96cdcc6d46f09aae051d684e019545841c785927
xMat27 <mathieu.belot34@bbox.fr>
2024-11-08 16:07:12 +0100

    modif taille bloc

--- Code/src/model.py
+++ Code/src/model.py
@@ -22,6 +22,7 @@ fps = int(cap.get(cv2.CAP_PROP_FPS))
 output_path = "video_flou.mp4"
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec vidéo
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+scale_factor = 1.3
 
 # Traitement de chaque frame
 while cap.isOpened():
@@ -34,13 +35,22 @@ while cap.isOpened():
 
     # Dessiner les boîtes englobantes sur la frame
     for box in results[0].boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])  # Extraire les coordonnées de la boîte
+         # Extraire les coordonnées et agrandir la boîte
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        w, h = x2 - x1, y2 - y1
+
+        # Calculer les nouvelles dimensions
+        x1 = max(0, int(x1 - (scale_factor - 1) * w / 2))
+        y1 = max(0, int(y1 - (scale_factor - 1) * h / 2))
+        x2 = min(width, int(x2 + (scale_factor - 1) * w / 2))
+        y2 = min(height, int(y2 + (scale_factor - 1) * h / 2))
+
 
         # Extraire la région d'intérêt (ROI) pour flouter
         roi = frame[(y1):(y2), (x1):(x2)]
 
         # Appliquer le flou sur cette région
-        blurred_roi = cv2.GaussianBlur(roi, (5, 5), 0)  # Utiliser un flou gaussien
+        blurred_roi = cv2.GaussianBlur(roi, (51, 51), 0)  # Utiliser un flou gaussien
         #temp = cv2.resize(roi, (4, 4), interpolation=cv2.INTER_LINEAR)
         #blurred_roi = cv2.resize(temp, (x2 - x1, y2 - y1), interpolation=cv2.INTER_NEAREST)
 
