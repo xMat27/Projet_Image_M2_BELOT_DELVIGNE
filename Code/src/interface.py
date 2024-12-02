37bff10891ae1f22427b38607a3e14e815c5215b
Brian <delvigne.brian@gmail.com>
2024-12-02 10:19:36 +0100

    finalisation poursuite de cible

--- Code/src/interface.py
+++ Code/src/interface.py
@@ -54,7 +54,7 @@ def reset_tracker(processed_frame, tracker_index, original_hist):
             best_box = bbox
 
     # Si une correspondance suffisante est trouvée, réinitialiser le tracker
-    if best_box and best_similarity > 0.8:  # Seuil de similarité ajustable
+    if best_box and best_similarity > 0.7:  # Seuil de similarité ajustable
         print(f"Réinitialisation du tracker {tracker_index}")
         trackers[tracker_index] = cv2.TrackerCSRT_create()
         trackers[tracker_index].init(processed_frame, best_box)
@@ -99,7 +99,7 @@ def load_video():
 
 # Traiter une vidéo
 def shuffle_video():
-    global video_capture, output_writer, processed_frame, video_boxes, frame_id,frame_counter
+    global video_capture, output_writer, processed_frame, video_boxes,frame_counter
     if not video_capture:
         return
 
@@ -120,10 +120,9 @@ def shuffle_video():
 
     video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
     video_boxes = []
-    frame_id = 0
 
     def update_frameS():
-        global frame, processed_frame, frame_id,frame_counter
+        global frame, processed_frame,frame_counter
         ret, frame = video_capture.read()
         
         
@@ -136,8 +135,7 @@ def shuffle_video():
             return
 
         processed_frame = frame.copy()
-        frame_counter+=1
-        frame_id += 1 
+        frame_counter+=1 
         frame_boxes = []
 
         if track.get() == 1 :        
@@ -172,7 +170,7 @@ def shuffle_video():
                         permutation = rng.permutation(len(pixels))  # Générer une permutation
                         pixels_mixed = pixels[permutation]          # Mélanger les pixels
                         processed_frame[y1:y2, x1:x2] = pixels_mixed.reshape(roi.shape)
-                    video_boxes.append({"frame_id": frame_id, "boxes": frame_boxes})         
+                    video_boxes.append({"frame_counter": frame_counter, "boxes": frame_boxes})         
             else:
                 for i, tracker in enumerate(trackers):
                     success, bbox = tracker.update(processed_frame)
@@ -181,17 +179,29 @@ def shuffle_video():
                         x2, y2 = x1 + w, y1 + h
                         frame_boxes.append({"x1": x1, "y1": y1, "x2": x2, "y2": y2})
 
-                        # Extraire la région d'intérêt
-                        roi = processed_frame[y1:y2, x1:x2]
+                        if frame_counter % 4 == 0:
+                            tracker_histograms[i] = calculate_histogram(processed_frame, (x1, y1, w, h))
 
-                        # Mélanger les pixels dans la boîte détectée
-                        pixels = roi.reshape(-1, 3)
-                        seed = 42  # Définir une graine
-                        rng = np.random.default_rng(seed)
-                        permutation = rng.permutation(len(pixels))  # Générer une permutation
-                        pixels_mixed = pixels[permutation]          # Mélanger les pixels
-                        processed_frame[y1:y2, x1:x2] = pixels_mixed.reshape(roi.shape)
-                video_boxes.append({"frame_id": frame_id, "boxes": frame_boxes}) 
+                        hist = calculate_histogram(processed_frame, bbox)
+                        similarity = compare_histograms(tracker_histograms[i], hist)
+
+                        # Seuil de similarité pour considérer la région comme valide
+                        print(f"similarity = {similarity}")
+                        if similarity < 0.8:
+                            print(f"Tracker {i}  a perdu la cible, tentative de réinitialisation...")
+                            reset_tracker(processed_frame, i, tracker_histograms[i])
+                            # Option : marquer la boîte ou réinitialiser le tracker
+                        else:
+                            roi = processed_frame[y1:y2, x1:x2]
+
+                            # Mélanger les pixels dans la boîte détectée
+                            pixels = roi.reshape(-1, 3)
+                            seed = 42  # Définir une graine
+                            rng = np.random.default_rng(seed)
+                            permutation = rng.permutation(len(pixels))  # Générer une permutation
+                            pixels_mixed = pixels[permutation]          # Mélanger les pixels
+                            processed_frame[y1:y2, x1:x2] = pixels_mixed.reshape(roi.shape)
+                        video_boxes.append({"frame_counter": frame_counter, "boxes": frame_boxes}) 
         else : 
             results = model(frame)  
             if int(user_Entry.get()) > 0:
@@ -213,7 +223,7 @@ def shuffle_video():
                 permutation = rng.permutation(len(pixels))  # Générer une permutation
                 pixels_mixed = pixels[permutation]          # Mélanger les pixels
                 processed_frame[y1:y2, x1:x2] = pixels_mixed.reshape(roi.shape) 
-                video_boxes.append({"frame_id": frame_id, "boxes": frame_boxes}) 
+                video_boxes.append({"frame_counter": frame_counter, "boxes": frame_boxes}) 
             else :
                 for box in results[0].boxes:
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -234,7 +244,7 @@ def shuffle_video():
                     permutation = rng.permutation(len(pixels))  # Générer une permutation
                     pixels_mixed = pixels[permutation]          # Mélanger les pixels
                     processed_frame[y1:y2, x1:x2] = pixels_mixed.reshape(roi.shape) 
-                video_boxes.append({"frame_id": frame_id, "boxes": frame_boxes})      
+                video_boxes.append({"frame_counter": frame_counter, "boxes": frame_boxes})      
         show_frame(processed_frame)
         root.after(30, update_frameS)
         output_writer.write(processed_frame)
@@ -249,7 +259,7 @@ def shuffle_video():
     
 
 def blur_video():
-    global video_capture, output_writer, processed_frame
+    global video_capture, output_writer, processed_frame, frame_counter
     if not video_capture:
         return
 
@@ -271,7 +281,7 @@ def blur_video():
     video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     def update_frameB():
-        global frame, processed_frame
+        global frame, processed_frame,frame_counter
         ret, frame = video_capture.read()
         
         if not ret:
@@ -282,6 +292,7 @@ def blur_video():
             return
 
         processed_frame = frame.copy()
+        frame_counter+=1 
 
         if track.get() == 1 :        
             if int(user_Entry.get()) > 0:
@@ -290,11 +301,24 @@ def blur_video():
                     x1, y1, w, h = map(int, bbox)
                     x2, y2 = x1 + w, y1 + h
 
-                    roi = processed_frame[y1:y2, x1:x2]
-                    # Mélange des pixels de la boîte (par exemple)
-                    blurred_roi = cv2.GaussianBlur(roi, (51, 51), 0)  
-            
-                    processed_frame[(y1):(y2), (x1):(x2)] = blurred_roi       
+                    if frame_counter % 4 == 0:
+                        tracker_histograms[int(user_Entry.get())-1] = calculate_histogram(processed_frame, (x1, y1, w, h))
+
+                    hist = calculate_histogram(processed_frame, bbox)
+                    similarity = compare_histograms(tracker_histograms[int(user_Entry.get())-1], hist)
+
+                    # Seuil de similarité pour considérer la région comme valide
+                    print(f"similarity = {similarity}")
+                    if similarity < 0.8:
+                        print(f"Tracker {int(user_Entry.get())-1}  a perdu la cible, tentative de réinitialisation...")
+                        reset_tracker(processed_frame, int(user_Entry.get())-1, tracker_histograms[int(user_Entry.get())-1])
+                        # Option : marquer la boîte ou réinitialiser le tracker
+
+                    else :
+                        roi = processed_frame[y1:y2, x1:x2]
+                        # Mélange des pixels de la boîte (par exemple)
+                        blurred_roi = cv2.GaussianBlur(roi, (51, 51), 0)  
+                        processed_frame[(y1):(y2), (x1):(x2)] = blurred_roi       
             else:
                 for i, tracker in enumerate(trackers):
                     success, bbox = tracker.update(processed_frame)
@@ -302,12 +326,26 @@ def blur_video():
                         x1, y1, w, h = map(int, bbox)
                         x2, y2 = x1 + w, y1 + h
 
-                        # Extraire la région d'intérêt
-                        roi = processed_frame[y1:y2, x1:x2]
-                        # Mélange des pixels de la boîte (par exemple)
-                        blurred_roi = cv2.GaussianBlur(roi, (51, 51), 0)  
-                
-                        processed_frame[(y1):(y2), (x1):(x2)] = blurred_roi
+                        if frame_counter % 4 == 0:
+                            tracker_histograms[i] = calculate_histogram(processed_frame, (x1, y1, w, h))
+
+                        hist = calculate_histogram(processed_frame, bbox)
+                        similarity = compare_histograms(tracker_histograms[i], hist)
+
+                        # Seuil de similarité pour considérer la région comme valide
+                        print(f"similarity = {similarity}")
+                        if similarity < 0.8:
+                            print(f"Tracker {i}  a perdu la cible, tentative de réinitialisation...")
+                            reset_tracker(processed_frame, i, tracker_histograms[i])
+                            # Option : marquer la boîte ou réinitialiser le tracker
+                        else :
+
+                            # Extraire la région d'intérêt
+                            roi = processed_frame[y1:y2, x1:x2]
+                            # Mélange des pixels de la boîte (par exemple)
+                            blurred_roi = cv2.GaussianBlur(roi, (51, 51), 0)  
+                    
+                            processed_frame[(y1):(y2), (x1):(x2)] = blurred_roi
         else :      
             results = model(frame)  
             if int(user_Entry.get()) > 0:
@@ -349,7 +387,7 @@ def blur_video():
 
 def pixel_video():
     
-    global video_capture, output_writer, processed_frame
+    global video_capture, output_writer, processed_frame,frame_counter
     if not video_capture:
         return
 
@@ -371,7 +409,7 @@ def pixel_video():
     video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     def update_frameP():
-        global frame, processed_frame
+        global frame, processed_frame,frame_counter
         ret, frame = video_capture.read()
         if not ret:
             video_capture.release()
@@ -379,6 +417,7 @@ def pixel_video():
             return
         
         processed_frame = frame.copy()
+        frame_counter+=1 
         if track.get() == 1 :        
             if int(user_Entry.get()) > 0:
                 success, bbox = trackers[int(user_Entry.get())-1].update(processed_frame)
@@ -386,11 +425,25 @@ def pixel_video():
                     x1, y1, w, h = map(int, bbox)
                     x2, y2 = x1 + w, y1 + h
 
-                    roi = processed_frame[y1:y2, x1:x2]
-                    # Mélange des pixels de la boîte (par exemple)
-                    temp = cv2.resize(roi, (4, 4), interpolation=cv2.INTER_LINEAR)
-                    pixel_roi = cv2.resize(temp, (x2 - x1, y2 - y1), interpolation=cv2.INTER_NEAREST)   
-                    processed_frame[(y1):(y2), (x1):(x2)] = pixel_roi  
+                    if frame_counter % 4 == 0:
+                        tracker_histograms[int(user_Entry.get())-1] = calculate_histogram(processed_frame, (x1, y1, w, h))
+
+                    hist = calculate_histogram(processed_frame, bbox)
+                    similarity = compare_histograms(tracker_histograms[int(user_Entry.get())-1], hist)
+
+                    # Seuil de similarité pour considérer la région comme valide
+                    print(f"similarity = {similarity}")
+                    if similarity < 0.8:
+                        print(f"Tracker {int(user_Entry.get())-1}  a perdu la cible, tentative de réinitialisation...")
+                        reset_tracker(processed_frame, int(user_Entry.get())-1, tracker_histograms[int(user_Entry.get())-1])
+                        # Option : marquer la boîte ou réinitialiser le tracker
+
+                    else :
+                        roi = processed_frame[y1:y2, x1:x2]
+                        # Mélange des pixels de la boîte (par exemple)
+                        temp = cv2.resize(roi, (4, 4), interpolation=cv2.INTER_LINEAR)
+                        pixel_roi = cv2.resize(temp, (x2 - x1, y2 - y1), interpolation=cv2.INTER_NEAREST)   
+                        processed_frame[(y1):(y2), (x1):(x2)] = pixel_roi  
             else:
                 for i, tracker in enumerate(trackers):
                     success, bbox = tracker.update(processed_frame)
@@ -398,11 +451,24 @@ def pixel_video():
                         x1, y1, w, h = map(int, bbox)
                         x2, y2 = x1 + w, y1 + h
 
-                        roi = processed_frame[y1:y2, x1:x2]
-                        # Mélange des pixels de la boîte (par exemple)
-                        temp = cv2.resize(roi, (4, 4), interpolation=cv2.INTER_LINEAR)
-                        pixel_roi = cv2.resize(temp, (x2 - x1, y2 - y1), interpolation=cv2.INTER_NEAREST) 
-                        processed_frame[(y1):(y2), (x1):(x2)] = pixel_roi
+                        if frame_counter % 4 == 0:
+                            tracker_histograms[i] = calculate_histogram(processed_frame, (x1, y1, w, h))
+
+                        hist = calculate_histogram(processed_frame, bbox)
+                        similarity = compare_histograms(tracker_histograms[i], hist)
+
+                        # Seuil de similarité pour considérer la région comme valide
+                        print(f"similarity = {similarity}")
+                        if similarity < 0.8:
+                            print(f"Tracker {i}  a perdu la cible, tentative de réinitialisation...")
+                            reset_tracker(processed_frame, i, tracker_histograms[i])
+                            # Option : marquer la boîte ou réinitialiser le tracker
+                        else :
+                            roi = processed_frame[y1:y2, x1:x2]
+                            # Mélange des pixels de la boîte (par exemple)
+                            temp = cv2.resize(roi, (4, 4), interpolation=cv2.INTER_LINEAR)
+                            pixel_roi = cv2.resize(temp, (x2 - x1, y2 - y1), interpolation=cv2.INTER_NEAREST) 
+                            processed_frame[(y1):(y2), (x1):(x2)] = pixel_roi
         else :      
             results = model(frame) 
             if int(user_Entry.get()) > 0:
@@ -446,7 +512,7 @@ def pixel_video():
     update_frameP()
 
 def dechiffre():
-    global video_capture, output_writer, processed_frame, video_boxes, frame_id, data
+    global video_capture, output_writer, processed_frame, video_boxes, frame_counter, data
     if not video_capture:
         return
 
@@ -467,19 +533,19 @@ def dechiffre():
 
     video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
     video_boxes = []
-    frame_id = 0
+    frame_counter = 0
 
     with open("video_boxes_coordinates.json", "r") as json_file:
         data = json.load(json_file)
 
-    def get_boxes_for_frame(frame_id, data):
+    def get_boxes_for_frame(frame_counter, data):
         for frame in data:
-            if frame["frame_id"] == frame_id:
+            if frame["frame_counter"] == frame_counter:
                 return frame["boxes"]  # Retourne la liste des boîtes
         return []  # Si aucune boîte n'est trouvée pour la frame
 
     def update_frameD():
-        global frame, processed_frame, frame_id, data
+        global frame, processed_frame, frame_counter, data
         ret, frame = video_capture.read()
         
         
@@ -493,8 +559,8 @@ def dechiffre():
 
         processed_frame = frame.copy()
 
-        frame_id += 1 
-        boxes = get_boxes_for_frame(frame_id, data)
+        frame_counter += 1 
+        boxes = get_boxes_for_frame(frame_counter, data)
         for box in boxes:
             x1, y1, x2, y2 = box["x1"], box["y1"], box["x2"], box["y2"]
 
